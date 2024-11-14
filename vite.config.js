0068bc918aa0db2dc902093e4953c9f0d1b79fd5
FridahWatetuMuthoni <fridahwatetumuthoni@gmail.com>
2024-11-14 10:22:53 +0300

    changed the vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
         target: "https://store-backend-7tlk.onrender.com",
+        changeOrigin: true, // changes the origin of the host header to the target URL
+        secure: false, // if you encounter issues with self-signed certificates
+        rewrite: (path) => path.replace(/^\/api/, ""), // rewrites /api to the backend path if needed
       },
     },
   },
